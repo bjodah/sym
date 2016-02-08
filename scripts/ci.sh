ceb76382a43237400394431918eda1e4b78920db
Björn Dahlgren <bjodah@gmail.com>
2016-02-08 17:23:29 +0100

    Add wrapper-class for SymCXX

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,7 +5,7 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
 fi
 
 # Py3
-conda create -q -n test3 python=3.4 symengine-python sympy pysym pip pytest pytest-cov pytest-flakes pytest-pep8
+conda create -q -n test3 python=3.4 symengine-python sympy pysym symcxx pip pytest pytest-cov pytest-flakes pytest-pep8
 source activate test3
 python setup.py install
 # (cd /; python -m pytest --pyargs $1)
@@ -14,7 +14,7 @@ PYTHONPATH=$(pwd) ./scripts/run_tests.sh --cov $1 --cov-report html
 #source deactivate
 
 # Py2
-conda create -q -n test2 python=2.7 symengine-python sympy pysym pip pytest
+conda create -q -n test2 python=2.7 symengine-python sympy pysym symcxx pip pytest
 source activate test2
 python setup.py sdist
 pip install dist/*.tar.gz
