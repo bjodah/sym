d6f79d4044628a44df0c2fc64f022a23ca52179d
Björn Dahlgren <bjodah@gmail.com>
2016-12-13 11:02:08 +0100

    Fix linear_rref test

--- sym/tests/test_util.py
+++ sym/tests/test_util.py
@@ -9,7 +9,7 @@ from .. import Backend
 @pytest.mark.parametrize('key', ['sympy'])
 def test_linear_rref(key):
     be = Backend(key)
-    A1 = [[8, 8, 4], [8, 8, 5], [2, 4, 4]]
+    A1 = be.Matrix([[8, 8, 4], [8, 8, 5], [2, 4, 4]])
     b1 = [80, 88, 52]
     A2, b2, colidxs = linear_rref(A1, b1, backend=be)
     A2ref = be.Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
