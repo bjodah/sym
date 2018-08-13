d5f06eceea9f655e51bff132e49380417c99a9ac
spcornelius <spcornelius@gmail.com>
2018-08-13 13:47:49 -0400

    Add CSC/CSR jacobian calculation to backends

--- sym/backend.py
+++ sym/backend.py
@@ -3,8 +3,9 @@ from __future__ import (absolute_import, division, print_function)
 
 import os
 import sys
+import numpy as np
 
-from .util import banded_jacobian
+from .util import banded_jacobian, sparse_jacobian_csc, sparse_jacobian_csr
 
 
 def _DenseMatrix(be, *args, **kwargs):
@@ -24,6 +25,22 @@ class _Base(object):
         """ Wraps Matrix around result of .util.banded_jacobian """
         exprs = banded_jacobian(exprs, dep, ml, mu)
         return self.Matrix(ml+mu+1, len(dep), list(exprs.flat))
+
+    def sparse_jacobian_csc(self, exprs, dep):
+        """ Wraps Matrix/ndarray around results of .util.sparse_jacobian_csc """
+        jac_exprs, colptrs, rowvals = sparse_jacobian_csc(exprs, dep)
+        nnz = len(jac_exprs)
+        return self.Matrix(1, nnz, jac_exprs),\
+               np.asarray(colptrs, dtype=int),\
+               np.asarray(rowvals, dtype=int)
+
+    def sparse_jacobian_csr(self, exprs, dep):
+        """ Wraps Matrix/ndarray around results of .util.sparse_jacobian_csr """
+        jac_exprs, rowptrs, colvals = sparse_jacobian_csr(exprs, dep)
+        nnz = len(jac_exprs)
+        return self.Matrix(1, nnz, jac_exprs),\
+               np.asarray(rowptrs, dtype=int),\
+               np.asarray(colvals, dtype=int)
 
 
 class _SymPy(_Base):
