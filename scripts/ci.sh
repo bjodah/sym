14bc5a159215a1f428c073356815166739ee9886
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-09 16:56:04 +0100

    symengine

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,6 +3,7 @@ if [[ "$DRONE_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
 fi
 
+python3 -m pip install symengine
 python3 -m pip install --user .[all]
 ./scripts/run_tests.sh
 ./scripts/render_notebooks.sh examples/
