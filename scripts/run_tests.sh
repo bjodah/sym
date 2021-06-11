a516c61d128c30b1888f9c1437be65037266709a
Björn Ingvar Dahlgren <bjodah@gmail.com>
2021-06-11 21:56:58 +0200

    flake8

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,5 +6,5 @@
 SYM_USE_CSE=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
 SYM_USE_NUMBA=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
 SYM_USE_CSE=1 SYM_USE_NUMBA=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
-${PYTHON:-python3} -m pytest --doctest-modules --pep8 --flakes $@
+${PYTHON:-python3} -m pytest --doctest-modules --flake8 $@
 ${PYTHON:-python3} -m doctest README.rst
