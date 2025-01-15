f05b2b8e2cca97edc37f6ef2682e014d2423d31f
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-15 17:07:20 +0100

    ci: split over multiple configs

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,13 +1,21 @@
 #!/bin/bash -xe
-if [[ "$DRONE_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
-    eval export ${1^^}_RELEASE_VERSION=\$CI_BRANCH
+
+PKG_NAME=${1:-${CI_REPO_NAME##*/}}
+
+if [[ "$CI_COMMIT_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
+    eval export ${PKG_NAME^^}_RELEASE_VERSION=\$CI_COMMIT_BRANCH
 fi
+
+REPO_TMP_DIR=$(mktemp -d)
+trap 'rm -rf -- "$REPO_TMP_DIR"' EXIT
+cp -ra . "$REPO_TMP_DIR/."
+cd "$REPO_TMP_DIR"
 
 python3 -m pip install symengine
 python3 -m pip install --no-build-isolation \
         "git+https://github.com/bjodah/symcxx#egg=symcxx" \
         "git+https://github.com/bjodah/pysym#egg=pysym"  # unofficial backends
-python3 -m pip install ${INSTALL_FLAGS_FOR_PIP} .[all]
+python3 -m pip install ${INSTALL_FLAGS_FOR_PIP:-} .[all]
 ./scripts/run_tests.sh -k "not diofant"
 ./scripts/render_notebooks.sh examples/
 ./scripts/generate_docs.sh
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,8 +3,6 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov sym --cov-report html
-SYM_USE_CSE=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
-SYM_USE_NUMBA=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
-SYM_USE_CSE=1 SYM_USE_NUMBA=1 ${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
+${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
 ${PYTHON:-python3} -m pytest --doctest-modules "$@"  # --flake8
 ${PYTHON:-python3} -m doctest README.rst
