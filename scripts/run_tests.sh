0f268b1cc8c9b55027025e1360fc5476a384002e
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-17 14:32:25 +0100

    update test script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,5 @@
 #   $ ./scripts/run_tests.sh
 # or
 #   $ ./scripts/run_tests.sh --cov sym --cov-report html
-${PYTHON:-python3} -m pytest sym/tests/test_Lambdify.py
 ${PYTHON:-python3} -m pytest --doctest-modules "$@"  # --flake8
 ${PYTHON:-python3} -m doctest README.rst
