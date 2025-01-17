c82ae79d2b3445a414d6472315bf8423603ff3b0
Björn Ingvar Dahlgren <bjodah@gmail.com>
2025-01-17 14:35:20 +0100

    ci: fix path

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,4 +4,4 @@
 # or
 #   $ ./scripts/run_tests.sh --cov sym --cov-report html
 ${PYTHON:-python3} -m pytest --doctest-modules "$@"  # --flake8
-${PYTHON:-python3} -m doctest README.rst
+${PYTHON:-python3} -m doctest $(dirname $BASH_SOURCE)/../README.rst
